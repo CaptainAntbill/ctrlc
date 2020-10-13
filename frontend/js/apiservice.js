956ed7f4418a3e8696b5cd40607e71716030901c
Anthony Mejia <mejiaanthony016@gmail.com>
2020-10-12 20:48:47 -0600

    Agregado el atributo precio a la vista de los servicios

--- frontend/js/apiservice.js
+++ frontend/js/apiservice.js
@@ -118,6 +118,8 @@ $(document).ready(function () {
                     "<tr><td>" + 
                     val.servicio + 
                     "</td><td>" + 
+                    val.precio + 
+                    "</td><td>" + 
                     "</tr>"
 
                 );
